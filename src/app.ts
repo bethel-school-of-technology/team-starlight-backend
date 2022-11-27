bd65891a97092564c1a15d27642992107b052c5a
reben87 <arebenst@gmail.com>
2022-11-27 07:53:37 -0800

    andrew-recipes

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 const cors = require('cors');
 const corsOptions = {
-    origin: [ 'http://localhost:3000 ']
+    origin:'http://localhost:3001', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
 };
 app.use(cors(corsOptions));
-// routes
+//routes
 app.use('/api/users', userRoutes);
 app.use('/api/recipe', recipeRoutes);
 
